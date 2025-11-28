458127122a879d88f9af07b0649bf40eb3ed9dbe
Anup Sebastian <56888132+anupjsebastian@users.noreply.github.com>
2025-11-28 09:59:03 -0600

    misc fixes

--- modules/music/bitwig.nix
+++ modules/music/bitwig.nix
@@ -1,21 +1,6 @@
 { unstablePkgs, pkgs, ... }:
-let
-  bitwig-wrapped = pkgs.symlinkJoin {
-    name = "bitwig-studio-wrapped";
-    paths = [ unstablePkgs.bitwig-studio ];
-    buildInputs = [ pkgs.makeWrapper ];
-    postBuild = ''
-      wrapProgram $out/bin/bitwig-studio \
-        --set GDK_BACKEND x11 \
-        --set GDK_SCALE 1 \
-        --set GDK_DPI_SCALE 1 \
-        --set QT_AUTO_SCREEN_SCALE_FACTOR 0 \
-        --set QT_SCALE_FACTOR 1
-    '';
-  };
-in
 {
   environment.systemPackages = [
-    bitwig-wrapped
+    unstablePkgs.bitwig-studio
   ];
 }
