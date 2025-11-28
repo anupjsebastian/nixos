0bd61b8be9b43ed0c925e9af4f5af8a5fae9495a
Anup Sebastian <56888132+anupjsebastian@users.noreply.github.com>
2025-11-27 22:27:06 -0600

    reverted fonts

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -3,24 +3,4 @@
   fonts.packages = [
     pkgs.nerd-fonts.zed-mono
   ];
-
-  fonts.fontconfig = {
-    defaultFonts = {
-      monospace = [ "ZedMono Nerd Font Mono" ];
-      sansSerif = [ "ZedMono Nerd Font Mono" ];
-      serif = [ "ZedMono Nerd Font Mono" ];
-    };
-  };
-
-  # Increase default font size for better readability
-  fonts.fontconfig.localConf = ''
-    <match target="pattern">
-      <test name="family">
-        <string>ZedMono Nerd Font Mono</string>
-      </test>
-      <edit name="pixelsize" mode="assign">
-        <double>16</double>
-      </edit>
-    </match>
-  '';
 }
